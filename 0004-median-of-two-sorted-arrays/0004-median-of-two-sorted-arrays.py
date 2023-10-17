ac54b362eef527fc21e450d24a31a439a55d1443
Awadhesh Bairagi <92313329+Awadheshbairagi@users.noreply.github.com>
2023-10-18 00:53:46 +0530

    Time: 93 ms (42.51%), Space: 17.6 MB (7.06%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -4,24 +4,19 @@ class Solution:
         n = na + nb
         
         def solve(k, a_start, a_end, b_start, b_end):
-            # If the segment of on array is empty, it means we have passed all
-            # its element, just return the corresponding element in the other array.
             if a_start > a_end: 
                 return B[k - a_start]
             if b_start > b_end: 
                 return A[k - b_start]
 
-            # Get the middle indexes and middle values of A and B.
             a_index, b_index = (a_start + a_end) // 2, (b_start + b_end) // 2
             a_value, b_value = A[a_index], B[b_index]
 
-            # If k is in the right half of A + B, remove the smaller left half.
             if a_index + b_index < k:
                 if a_value > b_value:
                     return solve(k, a_start, a_end, b_index + 1, b_end)
                 else:
                     return solve(k, a_index + 1, a_end, b_start, b_end)
-            # Otherwise, remove the larger right half. 
             else:
                 if a_value > b_value:
                     return solve(k, a_start, a_index - 1, b_start, b_end)
