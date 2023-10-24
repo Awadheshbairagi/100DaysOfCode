a4c66330f630c5c042010c406cfd33c49fdbdaa8
Awadhesh Bairagi <92313329+Awadheshbairagi@users.noreply.github.com>
2023-10-24 16:28:06 +0530

    Time: 41 ms (56.03%), Space: 16.3 MB (56.38%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.py
+++ 0101-symmetric-tree/0101-symmetric-tree.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -7,11 +9,18 @@
 
 class Solution:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
-        def isMirror(left, right):
+        if not root:
+            return True
+        
+        queue = deque([(root, root)])
+        
+        while queue:
+            left, right = queue.popleft()
             if not left and not right:
-                return True
+                continue
             if not left or not right or left.val != right.val:
                 return False
-            return isMirror(left.left, right.right) and isMirror(left.right, right.left)
+            queue.append((left.left, right.right))
+            queue.append((left.right, right.left))
         
-        return isMirror(root, root)
+        return True
