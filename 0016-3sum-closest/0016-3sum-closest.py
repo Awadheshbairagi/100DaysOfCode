505ed89ca5229df064e187a9a31c5fca30044dec
Awadhesh Bairagi <92313329+Awadheshbairagi@users.noreply.github.com>
2023-10-25 16:55:44 +0530

    Time: 670 ms (50.67%), Space: 16.5 MB (24.81%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.py
+++ 0016-3sum-closest/0016-3sum-closest.py
@@ -1,25 +1,20 @@
 class Solution:
     def threeSumClosest(self, nums: List[int], target: int) -> int:
-        nums.sort()  # Sort the input array
-        
-        closest_sum = float('inf')  # Initialize closest_sum to positive infinity
+        nums.sort() 
+        closest_sum = float('inf') 
         
         for i in range(len(nums) - 2):
-            left, right = i + 1, len(nums) - 1  # Initialize two pointers
+            left, right = i + 1, len(nums) - 1  
             
             while left < right:
-                total = nums[i] + nums[left] + nums[right]  # Calculate the current sum
-                
-                # Check if the current sum is closer to the target
+                total = nums[i] + nums[left] + nums[right] 
                 if abs(total - target) < abs(closest_sum - target):
                     closest_sum = total
                 
-                # Move pointers based on the comparison with the target
                 if total < target:
                     left += 1
                 elif total > target:
                     right -= 1
                 else:
-                    return closest_sum  # If the sum is equal to the target, return the sum as it can't get closer
-                
+                    return closest_sum  
         return closest_sum
