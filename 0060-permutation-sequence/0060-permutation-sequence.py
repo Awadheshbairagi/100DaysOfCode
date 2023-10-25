e82b418ec698c5b92ed9558e277a47ae3b9af358
Awadhesh Bairagi <92313329+Awadheshbairagi@users.noreply.github.com>
2023-10-25 19:17:01 +0530

    Time: 45 ms (33.99%), Space: 16.3 MB (62.04%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.py
+++ 0060-permutation-sequence/0060-permutation-sequence.py
@@ -1,22 +1,13 @@
 class Solution:
     def getPermutation(self, n: int, k: int) -> str:
-        # Calculate factorials for 1 to n
         factorial = [1]
         for i in range(1, n+1):
             factorial.append(factorial[-1] * i)
-        
-        # Create a list of available numbers to form permutations
         nums = [str(i) for i in range(1, n+1)]
-        
-        # Decrement k since the list is 0-indexed
         k -= 1
-        
-        # Build the permutation
         result = []
         for i in range(n, 0, -1):
             index = k // factorial[i - 1]
             k %= factorial[i - 1]
             result.append(nums.pop(index))
-        
-        # Convert the list to a string and return
         return ''.join(result)
