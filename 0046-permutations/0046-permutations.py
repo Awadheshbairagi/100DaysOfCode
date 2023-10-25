4f1bf824536417401d66b5f7806ebe5dc6608822
Awadhesh Bairagi <92313329+Awadheshbairagi@users.noreply.github.com>
2023-10-25 19:19:01 +0530

    Time: 45 ms (55.66%), Space: 16.5 MB (49.85%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,20 +1,14 @@
 from typing import List
-
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         def backtrack(first):
-            # If all integers are used up, add the permutation to the result
             if first == n:
                 output.append(nums[:])
                 return
             for i in range(first, n):
-                # Place the current integer at the first position
                 nums[first], nums[i] = nums[i], nums[first]
-                # Recursively generate permutations for the remaining integers
                 backtrack(first + 1)
-                # Undo the swap for backtracking
                 nums[first], nums[i] = nums[i], nums[first]
-        
         n = len(nums)
         output = []
         backtrack(0)
