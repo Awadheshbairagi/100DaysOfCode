de22d17db77e63b45595ae073bb8a59a71c72c1f
Awadhesh Bairagi <92313329+Awadheshbairagi@users.noreply.github.com>
2023-10-28 18:16:01 +0530

    Time: 51 ms (47.48%), Space: 16.5 MB (28.44%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.py
+++ 0010-regular-expression-matching/0010-regular-expression-matching.py
@@ -1,6 +1,7 @@
 class Solution(object):
     def isMatch(self, text, pattern):
         memo = {}
+
         def dp(i, j):
             if (i, j) not in memo:
                 if j == len(pattern):
@@ -8,7 +9,7 @@ class Solution(object):
                 else:
                     first_match = i < len(text) and pattern[j] in {text[i], '.'}
                     if j+1 < len(pattern) and pattern[j+1] == '*':
-                        ans = dp(i, j+2) or first_match and dp(i+1, j)
+                        ans = dp(i, j+2) or (first_match and dp(i+1, j))
                     else:
                         ans = first_match and dp(i+1, j+1)
 
