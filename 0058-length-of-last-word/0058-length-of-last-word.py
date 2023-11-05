5f38fb0e28053726e3a7f1f049e6f08de82c35d9
Awadhesh Bairagi <92313329+Awadheshbairagi@users.noreply.github.com>
2023-11-05 18:46:58 +0530

    Time: 42 ms (24.91%), Space: 16.3 MB (60.21%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.py
+++ 0058-length-of-last-word/0058-length-of-last-word.py
@@ -1,6 +1,6 @@
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
-        arr=s.split()
-        x=arr[len(arr)-1]
-        
-        return len(x)
+        words = s.split()  # Split the input string by spaces
+        if not words:  # If there are no words, return 0
+            return 0
+        return len(words[-1])  # Return the length of the last word
