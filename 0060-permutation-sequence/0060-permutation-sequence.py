a4930dfde7361edf1ffc7d312501efd0ca46a524
Awadhesh Bairagi <92313329+Awadheshbairagi@users.noreply.github.com>
2023-11-05 18:49:19 +0530

    Time: 49 ms (22.62%), Space: 16.1 MB (97.12%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.py
+++ 0060-permutation-sequence/0060-permutation-sequence.py
@@ -1,13 +1,16 @@
 class Solution:
     def getPermutation(self, n: int, k: int) -> str:
-        factorial = [1]
-        for i in range(1, n+1):
-            factorial.append(factorial[-1] * i)
-        nums = [str(i) for i in range(1, n+1)]
-        k -= 1
-        result = []
-        for i in range(n, 0, -1):
-            index = k // factorial[i - 1]
-            k %= factorial[i - 1]
-            result.append(nums.pop(index))
-        return ''.join(result)
+        total, ret, digits = 1, "", [i for i in range(1, n+1)]
+        for i in range(2, n):
+            total*= i
+        k-=1
+
+        for i in range(n-1, -1, -1):
+            #choose digit and remove it from digits
+            val = k // total
+            ret+= str(digits[val])
+            del digits[val]
+            k-= val*total
+
+            total = total//i if i>0 else total
+        return ret
