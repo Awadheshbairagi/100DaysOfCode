2447837c82d0cc41fa4ae6b651c23c4e9dd2ce01
Awadhesh Bairagi <92313329+Awadheshbairagi@users.noreply.github.com>
2023-11-08 15:30:39 +0530

    Time: 43 ms (58.61%), Space: 16.5 MB (25.01%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,22 +3,20 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        # Initialize pointers for nums1, nums2, and the merged array
+        # Initialize pointers for nums1 and nums2, starting from the end.
         p1, p2, p = m - 1, n - 1, m + n - 1
         
-        # Merge nums1 and nums2 starting from the end
+        # Compare elements from nums1 and nums2 and place the larger element in nums1.
         while p1 >= 0 and p2 >= 0:
-            # Compare the elements from the end of nums1 and nums2
             if nums1[p1] > nums2[p2]:
                 nums1[p] = nums1[p1]
                 p1 -= 1
             else:
                 nums1[p] = nums2[p2]
                 p2 -= 1
-            # Move the pointer for the merged array to the left
             p -= 1
         
-        # If there are remaining elements in nums2, copy them to nums1
+        # If there are remaining elements in nums2, place them in nums1.
         while p2 >= 0:
             nums1[p] = nums2[p2]
             p2 -= 1
