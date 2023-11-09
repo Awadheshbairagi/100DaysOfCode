cb1a93a7f82b7e79643fbf89a76960ccfb35b978
Awadhesh Bairagi <92313329+Awadheshbairagi@users.noreply.github.com>
2023-11-10 00:40:44 +0530

    Time: 57 ms (20.71%), Space: 16.6 MB (51.56%) - LeetHub

---
+++ 0097-interleaving-string/0097-interleaving-string.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+        if len(s1) + len(s2) != len(s3):
+            return False
+        
+        dp = [[False] * (len(s2) + 1) for _ in range(len(s1) + 1)]
+        dp[0][0] = True
+        
+        # Initialize the first row
+        for i in range(1, len(s2) + 1):
+            dp[0][i] = dp[0][i - 1] and s2[i - 1] == s3[i - 1]
+        
+        # Initialize the first column
+        for i in range(1, len(s1) + 1):
+            dp[i][0] = dp[i - 1][0] and s1[i - 1] == s3[i - 1]
+        
+        # Fill the DP table
+        for i in range(1, len(s1) + 1):
+            for j in range(1, len(s2) + 1):
+                dp[i][j] = (dp[i - 1][j] and s1[i - 1] == s3[i + j - 1]) or \
+                           (dp[i][j - 1] and s2[j - 1] == s3[i + j - 1])
+        
+        return dp[len(s1)][len(s2)]
