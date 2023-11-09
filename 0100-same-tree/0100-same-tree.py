a14f782dcf6944d7feee3860872a21a317712a0a
Awadhesh Bairagi <92313329+Awadheshbairagi@users.noreply.github.com>
2023-11-10 00:44:17 +0530

    Time: 37 ms (67.20%), Space: 16.3 MB (22.74%) - LeetHub

--- 0100-same-tree/0100-same-tree.py
+++ 0100-same-tree/0100-same-tree.py
@@ -7,11 +7,11 @@
 
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        # Base case: if both nodes are None, they are equal
+        # Base case: If both nodes are None, they are the same
         if not p and not q:
             return True
-        # If one node is None while the other is not, they are not equal
+        # If one of the nodes is None and the other is not, they are not the same
         if not p or not q:
             return False
-        # Check if the values are equal and recursively check left and right subtrees
+        # Check if current nodes have the same value and recursively check their left and right subtrees
         return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
