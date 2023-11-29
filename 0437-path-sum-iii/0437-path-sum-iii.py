99652bce1ffa9942ae8523465cde998122fa779f
Awadhesh Bairagi <92313329+Awadheshbairagi@users.noreply.github.com>
2023-11-30 01:17:22 +0530

    Time: 704 ms (6.76%), Space: 17.7 MB (76.82%) - LeetHub

---
+++ 0437-path-sum-iii/0437-path-sum-iii.py
@@ -0,0 +1,35 @@
+class Solution(object):
+    def pathSum(self, root, target):
+        """
+        :type root: TreeNode
+        :type sum: int
+        :rtype: int
+        """
+        # define global return var
+        self.numOfPaths = 0
+        # 1st layer DFS to go through each node
+        self.dfs(root, target)
+        # return result
+        return self.numOfPaths
+    
+    # define: traverse through the tree, at each treenode, call another DFS to test if a path sum include the answer
+    def dfs(self, node, target):
+        # exit condition
+        if node is None:
+            return 
+        # dfs break down 
+        self.test(node, target) # you can move the line to any order, here is pre-order
+        self.dfs(node.left, target)
+        self.dfs(node.right, target)
+        
+    # define: for a given node, DFS to find any path that sum == target, if find self.numOfPaths += 1
+    def test(self, node, target):
+        # exit condition
+        if node is None:
+            return
+        if node.val == target:
+            self.numOfPaths += 1
+            
+        # test break down
+        self.test(node.left, target-node.val)
+        self.test(node.right, target-node.val)
