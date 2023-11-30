659b3f011240445936e3407eb009842ab2f70e57
Awadhesh Bairagi <92313329+Awadheshbairagi@users.noreply.github.com>
2023-11-30 17:06:49 +0530

    Time: 112 ms (30.24%), Space: 78.9 MB (26.37%) - LeetHub

--- 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
@@ -1,8 +1,13 @@
 class Solution {
-public:
     int count = 0;
-    void dfs(int node, int parent, vector<vector<pair<int, int>>>& adj) {
-        for (auto& [child, sign] : adj[node]) {
+
+    public void dfs(int node, int parent, Map<Integer, List<List<Integer>>> adj) {
+        if (!adj.containsKey(node)) {
+            return;
+        }
+        for (List<Integer> nei : adj.get(node)) {
+            int child = nei.get(0);
+            int sign = nei.get(1);
             if (child != parent) {
                 count += sign;
                 dfs(child, node, adj);
@@ -10,13 +15,15 @@ public:
         }
     }
 
-    int minReorder(int n, vector<vector<int>>& connections) {
-        vector<vector<pair<int, int>>> adj(n);
-        for (auto& connection : connections) {
-            adj[connection[0]].push_back({connection[1], 1});
-            adj[connection[1]].push_back({connection[0], 0});
+    public int minReorder(int n, int[][] connections) {
+        Map<Integer, List<List<Integer>>> adj = new HashMap<>();
+        for (int[] connection : connections) {
+            adj.computeIfAbsent(connection[0], k -> new ArrayList<List<Integer>>()).add(
+                    Arrays.asList(connection[1], 1));
+            adj.computeIfAbsent(connection[1], k -> new ArrayList<List<Integer>>()).add(
+                    Arrays.asList(connection[0], 0));
         }
         dfs(0, -1, adj);
         return count;
     }
-};
+}
