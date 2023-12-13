0ae574b1ec8cee176697edf34d4d7bc5b6ba6321
Awadhesh Bairagi <92313329+Awadheshbairagi@users.noreply.github.com>
2023-12-13 11:46:35 +0530

    Time: 587 ms (89.22%), Space: 0B (100.00%) - LeetHub

---
+++ 0177-nth-highest-salary/0177-nth-highest-salary.sql
@@ -0,0 +1,10 @@
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+DECLARE M INT;
+SET M =N-1;
+  RETURN (
+   
+      IFNULL((SELECT DISTINCT salary FROM Employee ORDER BY salary DESC LIMIT M, 1),NULL)
+      
+       );
+END
