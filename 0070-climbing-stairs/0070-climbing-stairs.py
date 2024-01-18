336db6b8d4e6fc0ee62dc165dfaa5af9dc3d4fce
Awadhesh Bairagi <92313329+Awadheshbairagi@users.noreply.github.com>
2024-01-18 12:52:39 +0530

    Time: 46 ms (13.96%), Space: 17.3 MB (31.72%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,14 +1,8 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        # Initialize a list to store the number of ways to reach each step
         dp = [0] * (n + 1)
-        
-        # Base cases
         dp[0] = 1
         dp[1] = 1
-        
-        # Fill the DP table considering the two options: climb 1 step or climb 2 steps
         for i in range(2, n + 1):
             dp[i] = dp[i - 1] + dp[i - 2]
-        
         return dp[n]
