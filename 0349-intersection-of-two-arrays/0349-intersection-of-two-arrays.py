5df1c1baf5a04251ddd385c4161b60c5bb7dda00
Awadhesh Bairagi <92313329+Awadheshbairagi@users.noreply.github.com>
2024-03-10 23:01:35 +0530

    Time: 42 ms (86.67%), Space: 16.7 MB (53.45%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.py
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.py
@@ -1,17 +1,38 @@
-class Solution:
-    def set_intersection(self, set1, set2):
-        return [x for x in set1 if x in set2]
-        
+class Solution(object):
     def intersection(self, nums1, nums2):
         """
         :type nums1: List[int]
         :type nums2: List[int]
         :rtype: List[int]
-        """  
-        set1 = set(nums1)
-        set2 = set(nums2)
-        
-        if len(set1) < len(set2):
-            return self.set_intersection(set1, set2)
-        else:
-            return self.set_intersection(set2, set1)
+        """
+        # Sort both arrays
+        nums1.sort()
+        nums2.sort()
+
+        # Initialize two pointers
+        N = len(nums1)
+        M = len(nums2)
+        p1 = 0
+        p2 = 0
+
+        # Create set that stores integers appearing in both arrays
+        intersection = set()
+
+        while p1 < N and p2 < M:
+            # Add a value to the set if values at both pointers equal
+            if nums1[p1] == nums2[p2]:
+                intersection.add(nums1[p1])
+                p1 += 1
+                p2 += 1
+            elif nums1[p1] < nums2[p2]:
+                p1 += 1
+            else:
+                p2 += 1
+
+        # Convert intersection to an array
+        result = []
+        for x in intersection:
+            result.append(x)
+
+        # Return the result
+        return result
