08a2b444646e7971d1cb09319407faeb08d233d9
Awadhesh Bairagi <92313329+Awadheshbairagi@users.noreply.github.com>
2024-03-22 17:13:54 +0530

    Time: 275 ms (86.50%), Space: 32 MB (85.66%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -3,33 +3,28 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        # Helper function to reverse a linked list
-        def reverseList(node):
-            prev, curr = None, node
-            while curr:
-                next_temp = curr.next
-                curr.next = prev
-                prev = curr
-                curr = next_temp
-            return prev
-        
-        # Find the midpoint of the linked list
-        slow = fast = head
+
+        if head.next == None:
+            return True 
+        slow, fast, prev = head, head, None
         while fast and fast.next:
-            slow = slow.next
             fast = fast.next.next
-        
-        # Reverse the second half of the linked list
-        second_half = reverseList(slow)
-        
-        # Compare the first half and reversed second half
-        while second_half:
-            if head.val != second_half.val:
+            prev = slow
+            slow = slow.next
+        center    = slow    
+        prev.next = None  
+        prev, current = None, center
+        while(current):
+            following    = current.next
+            current.next = prev
+            prev         = current
+            current      = following
+        inversed = prev    
+        while head:
+            if head.val!=inversed.val:
                 return False
-            head = head.next
-            second_half = second_half.next
-        
+            head     = head.next
+            inversed = inversed.next
         return True
