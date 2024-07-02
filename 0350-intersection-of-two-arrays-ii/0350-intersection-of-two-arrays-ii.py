088576190867cff2c21bb0ba1fe0b0e15d4a44ea
Awadhesh Bairagi <92313329+Awadheshbairagi@users.noreply.github.com>
2024-07-02 19:30:27 +0530

    Time: 57 ms (21.07%), Space: 16.8 MB (31.35%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.py
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.py
@@ -1,20 +1,11 @@
 class Solution:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        # Create dictionaries to store the frequency of elements in nums1 and nums2
-        freq1 = {}
-        freq2 = {}
+        counter1 = Counter(nums1)
+        counter2 = Counter(nums2)
+        result = []
+        for num in counter1:
+            if num in counter2:
+                min_count = min(counter1[num], counter2[num])
+                result.extend([num] * min_count)
         
-        # Populate the frequency dictionaries
-        for num in nums1:
-            freq1[num] = freq1.get(num, 0) + 1
-        for num in nums2:
-            freq2[num] = freq2.get(num, 0) + 1
-        
-        # Find the intersection of elements based on the minimum frequency in both arrays
-        intersection = []
-        for num in freq1.keys():
-            if num in freq2:
-                # Add the element to the intersection list the minimum number of times it appears in both arrays
-                intersection.extend([num] * min(freq1[num], freq2[num]))
-        
-        return intersection
+        return result
