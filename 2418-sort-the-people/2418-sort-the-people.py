25a231e19679d93d1ad7ded0dcb8b5039d696e7b
Awadhesh Bairagi <92313329+Awadheshbairagi@users.noreply.github.com>
2024-07-22 09:55:54 +0530

    Time: 109 ms (42.68%), Space: 16.9 MB (84.16%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -1,9 +1,7 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        # Combine names and heights into pairs and sort them based on heights in descending order
-        sorted_pairs = sorted(zip(names, heights), key=lambda x: x[1], reverse=True)
-        
-        # Extract the sorted names from the sorted pairs
-        sorted_names = [name for name, height in sorted_pairs]
-        
+        number_of_people = len(names)
+        height_to_name_map = dict(zip(heights, names))
+        sorted_heights = sorted(heights, reverse=True)
+        sorted_names = [height_to_name_map[height] for height in sorted_heights]
         return sorted_names
