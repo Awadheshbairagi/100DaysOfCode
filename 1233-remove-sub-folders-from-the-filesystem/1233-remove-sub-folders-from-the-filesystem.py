66e346612ad0fedab8806ec09e6a7d6bd6c5baac
Awadhesh Bairagi <92313329+Awadheshbairagi@users.noreply.github.com>
2024-10-25 15:32:04 +0530

    Time: 18 ms (98.79%), Space: 29.6 MB (93.10%) - LeetHub

--- 1233-remove-sub-folders-from-the-filesystem/1233-remove-sub-folders-from-the-filesystem.py
+++ 1233-remove-sub-folders-from-the-filesystem/1233-remove-sub-folders-from-the-filesystem.py
@@ -1,10 +1,8 @@
 class Solution:
     def removeSubfolders(self, folder: List[str]) -> List[str]:
-        folder.sort()  # Sort the folder paths lexicographically
-        result = [folder[0]]  # Initialize the result list with the first folder
-        
+        folder.sort()  
+        result = [folder[0]] 
         for path in folder[1:]:
-            if not path.startswith(result[-1] + '/'):  # Check if the path is not a prefix of the last folder in result
-                result.append(path)  # If not a prefix, add it to the result
-        
+            if not path.startswith(result[-1] + '/'):  
+                result.append(path)  
         return result
